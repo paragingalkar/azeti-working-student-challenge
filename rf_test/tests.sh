cb27703703a00f05b8318877a83ea3b5ac0228ca
Parag Ingalkar <ingalkarparag@gmail.com>
2024-07-13 17:26:39 +0200

    Added rf-test container waiting logic

--- rf_test/tests.sh
+++ rf_test/tests.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env sh
 
 # Hint: Add something here to wait until the server is ready
+server_url="http://server:80/ready"
+echo "Waiting for the server to start..."
+
+while ! wget -q --spider "$server_url"
+do
+    echo "server not ready"
+    sleep 5
+done
+
+echo "Server started"
 
 mkdir -p results
 robot -d results test-server.robot
